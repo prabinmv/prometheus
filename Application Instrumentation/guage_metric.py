85100a82e4ab127e649f1ce290132fe9be0f1ce1
prabinmv <prabinmv@live.in>
2025-05-23 16:53:58 +0530

    gauage

---
+++ Application Instrumentation/guage_metric.py
@@ -0,0 +1,22 @@
+import http.server
+import time
+# from os import WIFCONTINUED
+from prometheus_client import start_http_server,Gauge
+
+REQUEST_IN_PROGRESS = Gauge("request_inprogress", "No of live request on application")
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        REQUEST_IN_PROGRESS.inc()
+        time.sleep(5)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>First python Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Python application.</center></h2></body></html>", "utf-8"))
+        self.wfile.close
+        REQUEST_IN_PROGRESS.dec()
+
+if __name__ == "__main__":
+    start_http_server(5001)
+    server = http.server.HTTPServer(('localhost', 5000), HandleRequests)
+    server.serve_forever()    
