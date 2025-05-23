dd01af1075025e2cb604e3a7da05f2b882eb013c
prabinmv <prabinmv@live.in>
2025-05-23 11:28:23 +0530

    ch

--- Application Instrumentation/counter_metric.py
+++ Application Instrumentation/counter_metric.py
@@ -9,8 +9,8 @@ REQUEST_COUNT = Counter("app_request_count", "Total_http_request_count")
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # REQUEST_COUNT.inc()
-        REQUEST_COUNT.labels('get_function', self.path).inc()
+        REQUEST_COUNT.inc()
+        # REQUEST_COUNT.labels('get_function', self.path).inc()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
